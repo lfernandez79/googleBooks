5e31a92394fdcf355f2c2590493ca32102b72775
LeoFdz <lfernandez79@gmail.com>
2020-04-01 22:36:06 -0500

    npm gh-pages

---
+++ client/build/precache-manifest.ecad7f83e129166692b0f8d71ba59e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "297cdd734ccf705f6a4b",
+    "url": "/static/css/main.74dc37fd.chunk.css"
+  },
+  {
+    "revision": "297cdd734ccf705f6a4b",
+    "url": "/static/js/main.7b152b9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e83e95dff7752b8a692",
+    "url": "/static/js/2.d0f67523.chunk.js"
+  },
+  {
+    "revision": "840c464cb0f763d5eaf7b0473ef27365",
+    "url": "/index.html"
+  }
+];
